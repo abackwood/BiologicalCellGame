81223131bb7a9697da8d080e8a004a8f9be4b390
abackwood <a.backwood@gmail.com>
2016-12-27 01:02:58 +0100

    Rudimentary player tracking with camera - PlayerMovement script provides stand-in player movement with WASD - CameraPlayerTracking script makes camera object follow player position in a smooth curve - Player and camera objects set up to show the effect of this

---
+++ Assets/CameraPlayerTracking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerTracking : MonoBehaviour {
+    public Transform playerTransform;
+
+    public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        velocity = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 difference = playerTransform.position - transform.position;
+        transform.position = Vector3.SmoothDamp(transform.position, playerTransform.position, ref velocity, 0.5f);
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDelta = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)) {
+            moveDelta += Vector3.forward;
+        }
+
+        if(Input.GetKey(KeyCode.S)) {
+            moveDelta += Vector3.back;
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            moveDelta += Vector3.left;
+        }
+
+        if(Input.GetKey(KeyCode.D)) {
+            moveDelta += Vector3.right;
+        }
+
+        transform.position += moveDelta.normalized * moveSpeed * Time.deltaTime;
+	}
+}
